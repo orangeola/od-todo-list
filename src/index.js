e5bc3691e4f949fd29c7649282552561767e9200
orangeola <orangeqmm@gmail.com>
2022-08-13 21:51:10 +0930

    Add basic styling

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 import './style.css';
 
-alert("tet");
+console.log("test");
+
+const content = document.createElement("div");
+content.setAttribute('id','content');
+
+const main = document.createElement("div");
+main.setAttribute('id','main');
+main.innerText = "Moeny";
+
+const header = document.createElement("div");
+header.setAttribute('id','header');
+header.innerText = "To Do List"
+
+const sideBar = document.createElement("div");
+sideBar.setAttribute('id','sideBar');
+sideBar.innerText = "Inbox";
+
+document.body.appendChild(header);
+
+content.appendChild(sideBar);
+content.appendChild(main);
+
+document.body.appendChild(content);
